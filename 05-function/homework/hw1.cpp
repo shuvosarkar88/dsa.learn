738aefd5943a7220085855d8d423766bed5a96e8
shuvo sarkar <shuvosarkar.dev@gmail.com>
2025-01-09 20:26:01 +0530

    solve homeWork 1

--- 05-function/homework/hw1.cpp
+++ 05-function/homework/hw1.cpp
@@ -1,37 +1,29 @@
-// #include <iostream>
+#include <iostream>
+using namespace std;
 
-// bool isPrime(int n) {
-//     if (n <= 1) {
-//         return false; // Numbers less than or equal to 1 are not prime.
-//     }
-//     if (n == 2) {
-//         return true; // 2 is the only even prime number.
-//     }
-//     if (n % 2 == 0) {
-//         return false; // Even numbers other than 2 are not prime.
-//     }
-//     // Check divisors from 3 to sqrt(n).
-//     for (int i = 3; i * i <= n; i += 2) {
-//         if (n % i == 0) {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
+int main() {
+  int n = 22;
 
-// int main() {
-//     int num;
-//     std::cout << "Enter a number: ";
-//     std::cin >> num;
+  if (n < 0) {
+    cout << "number less than 0 isn't prime\n";
+  } else if (n == 2) {
+    cout << n << " is prime\n";
+  } else if (n % 2 == 0) {
+    cout << n << " is not prime\n";
+  } else {
+    bool isPrime = true;
+    for (int i = 3; i <= n / 2; i++) {
+      if (n % i == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      cout << n << " is prime\n";
+    } else {
+      cout << n << " is not prime\n";
+    }
+  }
 
-//     if (isPrime(num)) {
-//         std::cout << num << " is prime" << std::endl;
-//     } else {
-//         std::cout << num << " is not prime" << std::endl;
-//     }
-
-//     return 0;
-// }
-
-
-
+  return 0;
+}
